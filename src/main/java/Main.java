32db129267fe53b4f3c965ac990c49c3053f4231
Peter Zaoral <pzaoral@redhat.com>
2017-08-23 10:57:27 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by pepo on 8/23/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Build successfully finished! BINGO!");
+    }
+}
