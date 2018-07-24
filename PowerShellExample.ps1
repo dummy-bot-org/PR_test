0b58a591e6eaea8b1476d115b2ebfe21fbc241a2
Peter Zaoral <pepo48@gmail.com>
2018-07-24 09:47:38 +0200

    Update PowerShellExample.ps1

--- PowerShellExample.ps1
+++ PowerShellExample.ps1
@@ -1,6 +1,4 @@
 #ps1_sysnative
 
-#Write-Output "----PUBLIC----"
-#git clone https://github.com/Pepo48/keycloak-quickstarts.git
-Write-Output "----PRIVATE----"
-git clone -q https://github.com/Pepo48/keycloak-bot.git
+#Write-Output "----IT IS WORKING----"
+
